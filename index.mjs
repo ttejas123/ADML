f4d3d150c2312ad4edda01a9c659146a2b98e7c8
Tejas <tthakare73@gmail.com>
2023-06-11 09:19:05 +0530

    Sss

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import clipboardy from 'clipboardy';
 import cors from 'cors'
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 // Define your APIs
